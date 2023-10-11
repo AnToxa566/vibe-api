eef1f77110ab02bc9031f7f0823fa3d5d64d14f8
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-10-11 17:13:09 +0300

    * add api global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { credentials: true, origin: '*' }, // process.env.CLIENT_URL
   });
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT);
 }
